551d5192a73ab4ab3abc3526704b25d873479253
Chris Beer <chris@cbeer.info>
2010-12-12 17:45:02 -0500

    add erc abbreviated forms

--- lib/anvl/erc.rb
+++ lib/anvl/erc.rb
@@ -1,88 +1,95 @@
 require 'anvl'
 module ANVL
   class Erc < Document
-  class Element < ANVL::Element
+    STORY_TO_OFFSET = { 'erc' => 0, 'about-erc' => 10, 'support-erc' => 20, 'meta-erc' => 30 }
 
-    def to_s
-      str = super
-      str &&= format_initial_comma_to_recover_word_order str
-      str
-    end
-
-    def to_anvl
-      "#{@display_label || @label}:#{format_value_for_anvl}"
-    end
-
-    private
-    def format_value_for_anvl
-      @value.gsub(/\n/, "\n    ").gsub(/^(\w)/, ' \1')
-    end
-
-    def format_initial_comma_to_recover_word_order str, sort_point = ','
-      return str unless str[0, 1] == sort_point
-      arr = str.split(sort_point)
-      arr.shift
-
-      insig = nil
-      if str[-1,1] == sort_point
-        insig = arr.pop.strip
-      end
-
-      word = arr.pop
-      arr = [arr.join ","]
-      arr.unshift word
-      arr.unshift insig
-
-      arr.compact.map(&:strip).reject { |x|  x.empty?}.join " "
-    end
-
-    def convert_label label
-      label.to_s.downcase.gsub(' ', '_')
-    end
-
-  end
     LABEL_TO_SYN = { 
-      'who' => 'h1', 'what' => 'h2', 'when' => 'h3', 'where' => 'h4'
+      'who' => 'h1', 'what' => 'h2', 'when' => 'h3', 'where' => 'h4', 'how' => 'h5',
+      'about-who' => 'h11', 'about-what' => 'h12', 'about-when' => 'h13', 'about-where' => 'h14', 'about-how' => 'h15',
+      'support-who' => 'h21', 'support-what' => 'h22', 'support-when' => 'h23', 'support-where' => 'h24',
+      'meta-who' => 'h31', 'meta-what' => 'h32', 'meta-when' => 'h33', 'meta-where' => 'h34'
     }
 
     SYN_TO_KERNEL = LABEL_TO_SYN.invert
 
-    INTERNATIONAL_LABELS = {
-      /\(h1\)/ => 'h1', /\(h2\)/ => 'h2', /\(h3\)/ => 'h3', /\(h4\)/ => 'h4'
-    }
 
-    def [] display_label
-      label = convert_label display_label
-      z = super label
-      z = super LABEL_TO_SYN[label] if z == []
-      z = super SYN_TO_KERNEL[label] if z == []
-
-      if z  == []
-        label_to_syn(label).each do |k, v|
-          z = super v if z == []
-          z = super SYN_TO_KERNEL[v] if z == []
-          break unless z == []
-        end
+    class Element < ANVL::Element
+      def to_s
+        str = super
+        str &&= format_initial_comma_to_recover_word_order str
+        str
       end
 
-      z
+      def =~ str
+        bool = super
+
+        label = convert_label str
+        bool ||= (label =~ Regexp.new("\\(#{Regexp.escape(@label)}\\)"))
+      end
+
+      private
+      def format_value_for_anvl
+        super.sub(/^\s+([,;\|])/, '\1')
+      end
+
+      def format_initial_comma_to_recover_word_order str, sort_point = ','
+        return str unless str[0, 1] == sort_point
+        arr = str.split(sort_point)
+        arr.shift
+
+       insig = nil
+        if str[-1,1] == sort_point
+          insig = arr.pop.strip
+        end
+
+        word = arr.pop
+        arr = [arr.join ","]
+        arr.unshift word
+        arr.unshift insig
+
+        arr.compact.map(&:strip).reject { |x|  x.empty?}.join " "
+      end
+
+      def convert_label label
+        str = label.to_s.downcase.gsub(' ', '_')
+
+        str = ANVL::Erc::LABEL_TO_SYN[str] if ANVL::Erc::LABEL_TO_SYN[str]
+
+        str
+      end
+
     end
 
-  #  def []= label, value
-  #   super convert_label(label), {:display_label => label, :value => value}
-  #  end
+    def []= display_label, value, append = false
+      if STORY_TO_OFFSET[display_label] and !value.strip.empty?
+        process_abbr_form display_label, value 
+      else
+        super
+      end
+    end
+    alias_method :store, :[]=
+
+    def complete?
+      !(self.fetch(:h1).empty? or self.fetch(:h2).empty? or self.fetch(:h3).empty? or self.fetch(:h4).empty?) ||
+      !(self.fetch(:h11).empty? or self.fetch(:h12).empty? or self.fetch(:h13).empty? or self.fetch(:h14).empty?)  ||
+      !(self.fetch(:h21).empty? or self.fetch(:h22).empty? or self.fetch(:h23).empty? or self.fetch(:h24).empty?) ||
+      !(self.fetch(:h31).empty? or self.fetch(:h32).empty? or self.fetch(:h33).empty? or self.fetch(:h34).empty?)
+    end
 
     protected
-    def label_to_syn label
-      INTERNATIONAL_LABELS.select { |k,v| k.is_a? Regexp }.select { |k,v| label.to_s =~ k }
-    end
 
     def element_class
       ANVL::Erc::Element
     end
 
-    def convert_label label
-      label.to_s.downcase.gsub(' ', '_')
+    def process_abbr_form display_label, value
+      offset = STORY_TO_OFFSET[display_label]
+      elements = value.split('|')
+      elements.each_with_index do |e, i|
+        next if e.strip.empty?
+        self.store "h#{offset + i + 1}", e.strip
+      end
     end
+
   end
 end
--- test/test_erc.rb
+++ test/test_erc.rb
@@ -9,15 +9,58 @@ class TestErc < Test::Unit::TestCase
 
   def test_basic
     str = 'erc:
-who:    Lederberg, Joshua
-what:   Studies of Human Families for Genetic Linkage
-when:   1974
-where:  http://profiles.nlm.nih.gov/BB/AA/TT/tt.pdf
-note:   This is an arbitrary note inside a
-        small descriptive record.'
+who:    Gibbon, Edward
+what:   The Decline and Fall of the Roman Empire
+when:   1781
+where:  http://www.ccel.org/g/gibbon/decline/'
+
     h = ANVL::Erc.parse str
     assert_equal("", h[:erc])
+    assert_equal("Gibbon, Edward", h[:who])
+    assert_equal("The Decline and Fall of the Roman Empire", h[:what])
+    assert_equal("1781", h[:when])
+    assert_equal("http://www.ccel.org/g/gibbon/decline/", h[:where])
+  end  
+
+  def test_complete
+    str = 'erc:
+what:    The Digital Dilemma
+where:  http://books.nap.edu/html/digital%5Fdilemma
+    '
+
+    h = ANVL::Erc.parse str
+    assert_equal(false, h.complete?)
+
+    h[:who] = '---'
+    h[:when] = '---'
+    assert(h.complete?)
   end
+
+  def test_complete_meta
+    str = 'meta-erc:  NLM | pm9546494 | 19980418
+               | http://ark.nlm.nih.gov/12025/pm9546494??'
+    h = ANVL::Erc.parse str
+    assert(h.complete?)
+  end
+
+  def test_missing_about
+    str = 'about-erc:   | Bispectrum ; Nonlinearity ; Epilepsy
+                   ; Cooperativity ; Subdural ; Hippocampus'
+    h = ANVL::Erc.parse str
+    assert_equal('Bispectrum ; Nonlinearity ; Epilepsy ; Cooperativity ; Subdural ; Hippocampus', h['about-what')
+  end
+
+  def test_abbr
+    str = 'erc: Gibbon, Edward | The Decline and Fall of the Roman Empire
+         | 1781 | http://www.ccel.org/g/gibbon/decline/'
+
+    h = ANVL::Erc.parse str
+    assert_equal("Gibbon, Edward", h[:who])
+    assert_equal("The Decline and Fall of the Roman Empire", h[:what])
+    assert_equal("1781", h[:when])
+    assert_equal("http://www.ccel.org/g/gibbon/decline/", h[:where])
+  end
+
 
   def test_intl
     str = 'erc:
@@ -33,7 +76,7 @@ h2:   Studies of Human Families for Genetic Linkage'
     assert_equal("Studies of Human Families for Genetic Linkage", h['was(h2)'])
   end
 
-  def test_comparison
+  def test_label_structure
     str = 'erc:'
     h = ANVL::Erc.parse str
 
@@ -43,5 +86,28 @@ h2:   Studies of Human Families for Genetic Linkage'
     h['MARC 856'] = '123'
 
     assert_equal("123", h['marc_856'])
+  end
+
+  def test_initial_comma
+    str = 'erc:
+who:,  van Gogh, Vincent
+who:,  Howell, III, PhD, 1922-1987, Thurston
+who:,  Acme Rocket Factory, Inc., The
+who:,  Mao Tse Tung
+who:,  McCartney, Pat, Ms,
+who:,  McCartney, Paul, Sir,
+who:,  McCartney, Petra, Dr,
+what:, Health and Human Services, United States Government
+    Department of, The,'
+
+    h = ANVL::Erc.parse str
+    assert_contains(h[:who], "Vincent van Gogh")
+    assert_contains(h[:who], "Thurston Howell, III, PhD, 1922-1987")
+    assert_contains(h[:who], "The Acme Rocket Factory, Inc.")
+    assert_contains(h[:who], "Mao Tse Tung")
+    assert_contains(h[:who], "Ms Pat McCartney")
+    assert_contains(h[:who], "Sir Paul McCartney")
+    assert_contains(h[:who], "Dr Petra McCartney")
+    assert_equal(h[:what], "The United States Government Department of Health and Human Services")
   end
 end
